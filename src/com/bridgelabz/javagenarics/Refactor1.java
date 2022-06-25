5be44e041672a4a49c458537f4e90f00b1f0a84e
srajashekhar <udayarajashekhar@gmail.com>
2022-06-25 09:15:28 +0530

    Added: Integer,floats & Strings to one generic method and find the maximum

---
+++ src/com/bridgelabz/javagenarics/Refactor1.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.javagenarics;
+
+public class Refactor1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Maximum number out of three integers is: " + maximum(11, 18, 200));
+		System.out.println("Maximum number out of three float is: " + maximum(45f, 75f, 88f));
+		System.out.println("Maximum among three string is: " + maximum("Apple", "Peach", "Banana"));
+	}
+
+	public static <T extends Comparable<T>> T maximum(T num1, T num2, T num3) {
+
+		if (num2.compareTo(num1) > 0) {
+			return num2;
+		} else if (num3.compareTo(num2) > 0) {
+			return num3;
+		} else {
+			return num1;
+		}
+
+	}
+}
